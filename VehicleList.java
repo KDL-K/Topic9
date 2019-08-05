711189232c687e729780caa8b123f8fdf5e16364
KDL-K <52216074+KDL-K@users.noreply.github.com>
2019-08-05 14:57:34 +0300

    VehicleList

---
+++ VehicleList.java
@@ -0,0 +1,10 @@
+package com.shevlik.Topic9;
+
+public interface VehicleList {
+    void showCarList();
+    void addToCarList();
+    void showCarByBrand(String brand);
+    void showCarByEngine(String engine);
+    void showCarByTransmission(String transmission);
+    void showCarById(int id);
+}
